87aa2289a5131f0674b8b50d3e441b875dad423d
santoshakkinen7@gmail.com <santoshakkinen7@gmail.com>
2022-02-05 15:52:34 -0300

    Init Project Java Spring Boot - First Commit

---
+++ src/main/java/br/com/diniz/desafio/JavaSpringBootApiRestWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.diniz.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootApiRestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootApiRestWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diniz/desafio/JavaSpringBootApiRestWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diniz.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootApiRestWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
